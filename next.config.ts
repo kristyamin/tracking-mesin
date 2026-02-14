c172c94a72fb5e656e0467a20b64b939d1c0a24e
kristyamin <kristyamin28@gmail.com>
2026-02-14 11:29:25 +0700

    off hp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',  // <--- WAJIB: Agar bisa jadi aplikasi HP (Offline mode)
+  //output: 'export',  // <--- WAJIB: Agar bisa jadi aplikasi HP (Offline mode)
   images: {
     unoptimized: true, // <--- WAJIB: Agar gambar Supabase muncul di HP
   },
